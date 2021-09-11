5a265b8416630c4d06764946aadc28c36a79b978
bitbeckers <code@bitbeckers.com>
2021-09-11 16:14:06 +0200

    feat: added basic web3modal implementation now autoconnects to MM

--- CommitPoolApp/components/web3-helper/web3-helper.js
+++ CommitPoolApp/components/web3-helper/web3-helper.js
@@ -2,14 +2,51 @@ import { ethers } from "ethers";
 import getEnvVars from "../../environment.js";
 import getContract from "../contract/contract";
 import Torus from "@toruslabs/torus-embed";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
 
 const {
   commitPoolContractAddress,
   daiContractAddress,
   abi,
   daiAbi,
-  torusLogging
+  torusLogging,
 } = getEnvVars();
+
+const providerOptions = {
+  torus: {
+    package: Torus,
+    options: {
+      networkParams: {
+        host: "https://polygon-mainnet.infura.io/v3/3c072dd341bb4e45858038e146195ae1", // optional
+        chainId: 137,
+        networkId: 137,
+        networkName: "Polygon Main Network",
+      },
+      config: {
+        buildEnv: "production",
+        showTorusButton: true,
+        enableLogging: torusLogging,
+      },
+    },
+  },
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      137: `https://polygon-mainnet.infura.io/v3/3c072dd341bb4e45858038e146195ae1`, // required
+    },
+  },
+};
+
+const connectProvider = async () => {
+  return await web3Modal.connect();
+};
+
+const web3Modal = new Web3Modal({
+  network: "mainnet", // optional
+  cacheProvider: true, // optional
+  providerOptions, // required
+});
 
 const web3Helper = {
   // account: undefined,
@@ -21,19 +58,21 @@ const web3Helper = {
     commitPool: {},
     dai: {},
   },
-  logOut: function(){
+  logOut: function () {
     web3Helper.torus.cleanUp();
     web3Helper.initialize();
   },
   provider: undefined,
-  setWeb3Provider: function () {
-    web3Helper.provider = new ethers.providers.Web3Provider(web3Helper.torus.provider);
+  setWeb3Provider: function (provider) {
+    web3Helper.provider = new ethers.providers.Web3Provider(
+      provider
+    );
     web3Helper.contracts.dai = getContract(
       daiContractAddress,
       daiAbi,
       web3Helper.provider
     );
-    
+
     web3Helper.contracts.commitPool = getContract(
       commitPoolContractAddress,
       abi,
@@ -43,23 +82,7 @@ const web3Helper = {
   torus: {},
   wallet: {},
   initialize: async function () {
-    const torus = new Torus({
-      buttonPosition: "bottom-left",
-    })
-    web3Helper.torus = torus;
-    await web3Helper.torus.init({
-      buildEnv: "production", 
-      enableLogging: torusLogging, 
-      network: {
-        host: "https://polygon-mainnet.infura.io/v3/3c072dd341bb4e45858038e146195ae1", 
-        chainId: 137, 
-        networkName: "Matic Main Network", 
-      },
-      showTorusButton: true,
-    });
-
-    await web3Helper.torus.login(); 
-    web3Helper.setWeb3Provider();
+    await connectProvider().then(this.setWeb3Provider);
     return web3Helper;
   },
 };

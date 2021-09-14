224518cb1ed0ef1139f003fa41c66d5c33ca4d97
bitbeckers <code@bitbeckers.com>
2021-09-14 13:51:03 +0200

    feat(web3modal): code cleanup in web3helper to improver init flow

--- CommitPoolApp/components/web3-helper/web3-helper.ts
+++ CommitPoolApp/components/web3-helper/web3-helper.ts
@@ -48,6 +48,76 @@ const defaultWeb3Modal = new Web3Modal({
   providerOptions, // required
 });
 
+const web3Helper = {
+  account: undefined,
+  contracts: undefined,
+  isLoggedIn: false,
+  logOut: async function () {
+    console.log("Web3Helper before closing: ", web3Helper);
+    if (web3Helper.provider && web3Helper.provider.close) {
+      await web3Helper.provider.close();
+    }
+    web3Helper.web3Modal.clearCachedProvider();
+    web3Helper.web3Modal = defaultWeb3Modal;
+    web3Helper.account = undefined;
+    web3Helper.provider = undefined;
+    web3Helper.contracts = undefined;
+    web3Helper.isLoggedIn = false;
+    window.localStorage.removeItem("WEB3_CONNECT_CACHED_PROVIDER");
+  },
+  provider: undefined,
+  web3Modal: defaultWeb3Modal,
+  connectProvider: async () => {
+    return await defaultWeb3Modal.connect();
+  },
+
+  initialize: async () => {
+    return await setWeb3Modal(web3Helper)
+      .then(setWeb3Provider)
+      .then(setAccount)
+      .then(setContracts)
+      .then(setLoggedIn);
+  },
+};
+
+const setAccount = (web3Helper) => {
+  if (web3Helper.provider) {
+    const account = deriveSelectedAddress(web3Helper.provider.provider);
+    web3Helper.account = account;
+  }
+  return web3Helper;
+};
+
+const setContracts = (web3Helper) => {
+  if (web3Helper.provider) {
+    const contracts = getContractsWithProvider(web3Helper.provider);
+    web3Helper.contracts = contracts;
+  }
+  return web3Helper;
+};
+
+const setLoggedIn = (web3Helper) => {
+  if (web3Helper.provider && web3Helper.account && web3Helper.contracts) {
+    web3Helper.isLoggedIn = true;
+  } else {
+    web3Helper.isLoggedIn = false;
+  }
+
+  return web3Helper;
+};
+
+const setWeb3Modal = async (web3Helper) => {
+  const localProvider = await web3Helper.connectProvider();
+  web3Helper.web3Modal = localProvider;
+  return web3Helper;
+};
+
+const setWeb3Provider = (web3Helper) => {
+  const localProvider = subscribeToEvents(web3Helper.web3Modal);
+  web3Helper.provider = new ethers.providers.Web3Provider(localProvider);
+  return web3Helper;
+};
+
 const deriveSelectedAddress = (provider: any) => {
   console.log(provider);
   if (provider.isMetaMask || provider.isTorus) {
@@ -61,87 +131,37 @@ const deriveSelectedAddress = (provider: any) => {
   return null;
 };
 
-const web3Helper = {
-  account: undefined,
-  connectProvider: async function () {
-    return await defaultWeb3Modal.connect();
-  },
-  setAccount: function (localProvider) {
-    web3Helper.account = deriveSelectedAddress(localProvider.provider);
-  },
-  contracts: {
-    commitPool: {},
-    dai: {},
-  },
-  isLoggedIn: false,
-  logOut: async function () {
-    console.log("Web3Helper before closing: ", web3Helper);
-    if (web3Helper.provider && web3Helper.provider.close) {
-      await web3Helper.provider.close();
-    }
-    web3Helper.web3Modal.clearCachedProvider();
-    web3Helper.web3Modal = defaultWeb3Modal;
-    web3Helper.account = undefined;
-    web3Helper.provider = undefined;
-    web3Helper.contracts = {
-      commitPool: {},
-      dai: {},
-    };
-    web3Helper.isLoggedIn = false;
-    window.localStorage.removeItem("WEB3_CONNECT_CACHED_PROVIDER");
-  },
-  provider: undefined,
-  web3Modal: defaultWeb3Modal,
-  setWeb3Provider: function (localProvider) {
-    // Subscribe to accounts change
-    localProvider.on("accountsChanged", (accounts: string[]) => {
-      console.log(accounts);
-    });
+const getContractsWithProvider = (provider) => {
+  const dai = getContract(daiContractAddress, daiAbi, provider);
+  const commitPool = getContract(commitPoolContractAddress, abi, provider);
 
-    // Subscribe to chainId change
-    localProvider.on("chainChanged", (chainId: number) => {
-      console.log(chainId);
-    });
+  return { dai, commitPool };
+};
 
-    // Subscribe to provider connection
-    localProvider.on("connect", (info: { chainId: number }) => {
-      console.log(info);
-    });
+const subscribeToEvents = (provider) => {
+  const localProvider = provider;
 
-    // Subscribe to provider disconnection
-    localProvider.on(
-      "disconnect",
-      (error: { code: number; message: string }) => {
-        console.log(error);
-      }
-    );
+  // Subscribe to accounts change
+  localProvider.on("accountsChanged", (accounts: string[]) => {
+    console.log(accounts);
+  });
 
-    web3Helper.provider = new ethers.providers.Web3Provider(localProvider);
+  // Subscribe to chainId change
+  localProvider.on("chainChanged", (chainId: number) => {
+    console.log(chainId);
+  });
 
-    web3Helper.isLoggedIn = true;
-    return web3Helper.provider;
-  },
-  setContracts: function () {
-    web3Helper.contracts.dai = getContract(
-      daiContractAddress,
-      daiAbi,
-      web3Helper.provider
-    );
+  // Subscribe to provider connection
+  localProvider.on("connect", (info: { chainId: number }) => {
+    console.log(info);
+  });
 
-    web3Helper.contracts.commitPool = getContract(
-      commitPoolContractAddress,
-      abi,
-      web3Helper.provider
-    );
-  },
-  initialize: async function () {
-    await web3Helper
-      .connectProvider()
-      .then((localProvider) => web3Helper.setWeb3Provider(localProvider))
-      .then((web3Provider) => web3Helper.setAccount(web3Provider))
-      .then(() => web3Helper.setContracts());
-    return web3Helper;
-  },
+  // Subscribe to provider disconnection
+  localProvider.on("disconnect", (error: { code: number; message: string }) => {
+    console.log(error);
+  });
+
+  return localProvider;
 };
 
 export default web3Helper;
